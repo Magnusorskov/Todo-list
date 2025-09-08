61f6f34f38a9aa27de9ae717053c730cd5e6fd40
Magnus Orskov <magnusorskov@hotmail.com>
2025-09-08 19:10:54 +0200

    add project module and create html skeleton

--- src/Project.js
+++ src/Project.js
@@ -1,1 +1,60 @@
-export default class Project {}
+import Todo from "./Todo";
+import { Urgency } from "./Urgency";
+import { calculateProgressInPercentage } from "./calculationUtils";
+
+export default class Project {
+    #name;
+    #todoLists;
+
+    constructor(name) {
+        this.#name = name;
+        this.#todoLists = [];
+    }
+
+    get name() {
+        return this.#name;
+    }
+
+    set name(value) {
+        this.#name = value;
+    }
+
+    get todoLists() {
+        return this.#todoLists;
+    }
+
+    createTodo(
+        title,
+        description = null,
+        dueDate = null,
+        urgency = Urgency.MEDIUM,
+        note = null
+    ) {
+        const newTodo = new Todo(title, description, dueDate, urgency, note);
+        this.#todoLists.push(newTodo);
+        return newTodo;
+    }
+
+    deleteTodo(todoToDelete) {
+        this.#todoLists = this.#todoLists.filter(
+            (todo) => todo !== todoToDelete
+        );
+    }
+
+    countProgress() {
+        return this.todoLists.reduce(
+            (accumulator, todoList) => {
+                const todoListProgress = todoList.countProgress();
+                accumulator.completed += todoListProgress.completed;
+                accumulator.inProgress += todoListProgress.inProgress;
+                return accumulator;
+            },
+            { completed: 0, inProgress: 0 }
+        );
+    }
+
+    calculateProgressInPercentage() {
+        return calculateProgressInPercentage(this.countProgress());
+    }
+
+}
