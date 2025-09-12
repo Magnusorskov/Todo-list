5e60a2aec407924b35ee28559a9e3ab2f2086d1b
Magnus Orskov <magnusorskov@hotmail.com>
2025-09-12 16:06:08 +0200

    add dialogs for adding projects and todos

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
 import "./styles.css";
 import "js-circle-progress";
+
+const addTodo = document.querySelector(".add-todo");
+const closeDialog = document.querySelector(".todo-dialog__button--cancel");
+const addTodoDialog = document.querySelector(".todo-dialog");
+
+addTodo.addEventListener("click", () => {
+    addTodoDialog.showModal();
+});
+
+closeDialog.addEventListener("click", () => {
+    addTodoDialog.close();
+});
+
+const addProject = document.querySelector(".add-project");
+const closeProjectDialog = document.querySelector(
+    ".Project-dialog__button--cancel"
+);
+const addProjectDialog = document.querySelector(".Project-dialog");
+
+addProject.addEventListener("click", () => {
+    addProjectDialog.showModal();
+});
+
+closeDialog.addEventListener("click", () => {
+    addProjectDialog.close();
+});
